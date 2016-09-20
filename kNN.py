24fd410e03da928fa443189217b405c044041f71
Your Namehaotin <616566709@qq.com>
2016-09-20 19:04:44 +0800

    Revert "branch demo"
    This reverts commit b4baaf534e8f9b127ac36e136420ac348dc296c3.

--- kNN.py
+++ kNN.py
@@ -3,7 +3,7 @@ import operator
 import matplotlib
 import matplotlib.pyplot as plt
 
-# branch demo
+
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
